ded0e02e8fa2b2a1480611d22afb8b9aaed7c5a2
Marco Tasca <tascamarcoo@gmail.com>
2023-11-13 15:48:51 +0100

    Batman! (this commit has no parents)

---
+++ src/main/java/com/candido/server/CandidoServerApplication.java
@@ -0,0 +1,13 @@
+package com.candido.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidoServerApplication.class, args);
+	}
+
+}
