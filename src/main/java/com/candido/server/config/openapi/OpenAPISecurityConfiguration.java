7ceab6f0befedc4d092fcbe114db6c6f6eaeca40
Marco Tasca <tascamarcoo@gmail.com>
2024-01-20 21:33:12 +0100

    feat: Add SpringDoc OpenAPI configuration

---
+++ src/main/java/com/candido/server/config/openapi/OpenAPISecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.candido.server.config.openapi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info =@Info(
+                title = "Candido API",
+                version = "${application.api.version}",
+                contact = @Contact(
+                        name = "Candido",
+                        email = "support@candido.com",
+                        url = "https://www.candido.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "${application.tos.uri}",
+                description = "${application.api.description}"
+        ),
+        servers = {
+                @Server(url = "${application.api.server.development.url}", description = "development"),
+                @Server(url = "${application.api.server.production.url}", description = "production")
+        }
+)
+public class OpenAPISecurityConfiguration {}
