a49f7dab6e5e3323f24da9276e987a00d823fa25
Marco Tasca <tascamarcoo@gmail.com>
2024-03-04 23:24:45 +0100

    feat: Add dto to update the user

---
+++ src/main/java/com/candido/server/dto/v1/request/account/RequestUpdateUserDto.java
@@ -0,0 +1,14 @@
+package com.candido.server.dto.v1.request.account;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+
+public record RequestUpdateUserDto (
+        @JsonProperty("first_name") String firstName,
+        @JsonProperty("last_name") String lastName,
+        @JsonProperty("gender_id") int genderId,
+        @JsonProperty("birthdate") LocalDateTime birthdate,
+        @JsonProperty("mobile_number") String mobileNumber,
+        @JsonProperty("phone_number") String phoneNumber
+) {}
