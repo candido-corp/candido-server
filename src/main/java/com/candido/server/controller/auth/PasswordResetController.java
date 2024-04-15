9ad69af83f442abfbdc8f7899fa2eef6e6d524b1
Marco Tasca <tascamarcoo@gmail.com>
2024-04-15 23:13:32 +0200

    fix: Refactor controller name and change import for authentication service

--- src/main/java/com/candido/server/controller/auth/PasswordResetController.java
+++ src/main/java/com/candido/server/controller/auth/PasswordResetController.java
@@ -3,43 +3,42 @@ package com.candido.server.controller.auth;
 import com.candido.server.domain.v1.token.TokenScopeCategoryEnum;
 import com.candido.server.dto.v1.request.auth.RequestPasswordReset;
 import com.candido.server.dto.v1.response.auth.ResponseAuthentication;
-import com.candido.server.service.auth.AuthenticationServiceImpl;
+import com.candido.server.service.auth.AuthenticationService;
 import com.candido.server.util.UtilService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthenticationAccountController {
+public class PasswordResetController {
 
-    private final AuthenticationServiceImpl authenticationServiceImpl;
+    private final AuthenticationService authenticationService;
 
-    @Autowired
-    UtilService utilService;
+    private final UtilService utilService;
 
-    @PostMapping("/reset-password-send/{email}")
+    @PostMapping("/reset-password/send/{email}")
     public ResponseEntity<Void> sendResetPassword(@PathVariable("email") String email, HttpServletRequest httpRequest) {
-        authenticationServiceImpl.sendResetPassword(email, utilService.getClientIP(httpRequest), utilService.getAppUrl(httpRequest));
+        authenticationService.sendResetPassword(email, utilService.getClientIP(httpRequest), utilService.getAppUrl(httpRequest));
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/reset-password-verify/{token}")
+    @PostMapping("/reset-password/verify/{token}")
     public ResponseEntity<Void> verifyResetToken(@PathVariable("token") String token) {
-        authenticationServiceImpl.getAccountAndVerifyToken(token, TokenScopeCategoryEnum.BTD_RESET.getTokenScopeCategoryId());
+        authenticationService.getAccountAndVerifyToken(token, TokenScopeCategoryEnum.BTD_RESET.getTokenScopeCategoryId());
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/reset-password/{token}")
     public ResponseEntity<ResponseAuthentication> resetPassword(
-            @RequestBody RequestPasswordReset request,
             @PathVariable("token") String token,
+            @RequestBody RequestPasswordReset request,
             HttpServletRequest httpRequest
     ) {
-        return ResponseEntity.ok(authenticationServiceImpl.resetPassword(token, request, utilService.getClientIP(httpRequest)));
+        ResponseAuthentication authentication = authenticationService.resetPassword(token, request, utilService.getClientIP(httpRequest));
+        return ResponseEntity.ok(authentication);
     }
 
 
