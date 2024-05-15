85a11c9aa4a2d3a207bea154ea3fe8cacb0eb870
Marco Tasca <tascamarcoo@gmail.com>
2024-05-15 17:56:11 +0200

    feat: Add Java doc to class OnRegistrationCompletedEvent

--- src/main/java/com/candido/server/event/auth/OnRegistrationCompletedEvent.java
+++ src/main/java/com/candido/server/event/auth/OnRegistrationCompletedEvent.java
@@ -5,13 +5,27 @@ import com.candido.server.domain.v1.user.User;
 import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
+/**
+ * This class represents the event that is published when a registration is completed.
+ * It extends the ApplicationEvent class provided by Spring Framework.
+ * It contains the account and user details associated with the registration.
+ */
 @Getter
 public class OnRegistrationCompletedEvent extends ApplicationEvent {
 
+    // The account associated with the registration
     private final Account account;
 
+    // The user associated with the registration
     private final User user;
 
+    /**
+     * Constructor for the OnRegistrationCompletedEvent.
+     *
+     * @param source  the object on which the event initially occurred
+     * @param account the account associated with the registration
+     * @param user    the user associated with the registration
+     */
     public OnRegistrationCompletedEvent(Object source, final Account account, final User user) {
         super(source);
         this.account = account;
