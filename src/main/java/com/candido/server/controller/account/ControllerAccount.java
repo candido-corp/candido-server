2765707077c4555df89c967c3327fec9799cc8f2
Marco Tasca <tascamarcoo@gmail.com>
2025-01-10 22:55:49 +0100

    feat: Remove services from account controller and add them to user controller

--- src/main/java/com/candido/server/controller/account/ControllerAccount.java
+++ src/main/java/com/candido/server/controller/account/ControllerAccount.java
@@ -1,10 +1,7 @@
 package com.candido.server.controller.account;
 
 import com.candido.server.domain.v1.account.Account;
-import com.candido.server.domain.v1.user.User;
-import com.candido.server.dto.v1.request.account.RequestUpdateUserDto;
 import com.candido.server.dto.v1.util.AccountDto;
-import com.candido.server.dto.v1.util.UserDto;
 import com.candido.server.service.base.account.AccountService;
 import com.candido.server.service.base.mapper.AccountMapperService;
 import com.candido.server.service.base.mapper.UserMapperService;
@@ -20,8 +17,6 @@ public class ControllerAccount {
 
     private final AccountService accountService;
     private final AccountMapperService accountMapper;
-    private final UserMapperService userMapper;
-    private final UserService userService;
 
     @Autowired
     public ControllerAccount(
@@ -32,36 +27,12 @@ public class ControllerAccount {
     ) {
         this.accountService = accountService;
         this.accountMapper = accountMapper;
-        this.userMapper = userMapper;
-        this.userService = userService;
     }
 
     @GetMapping
     public ResponseEntity<AccountDto> getAccountInfo(Authentication authentication) {
         Account account = accountService.findAccountByEmailOrThrow(authentication.getName());
         return ResponseEntity.ok(accountMapper.accountToAccountDto(account));
-    }
-
-    @GetMapping("/details")
-    public ResponseEntity<UserDto> getUserInfo(Authentication authentication) {
-        Account account = accountService.findAccountByEmailOrThrow(authentication.getName());
-        User user = userService.findUserByAccountIdOrThrow(account.getId());
-        UserDto userDto = userMapper.userToUserDto(user);
-        return ResponseEntity.ok(userDto);
-    }
-
-    @PutMapping("/details")
-    public ResponseEntity<UserDto> postUserInfo(
-            Authentication authentication,
-            @RequestBody RequestUpdateUserDto requestUpdateUserDto
-    ) {
-        // TODO: Se c'è una candidatura aperta non può modificare il nome e cognome.
-
-        Account account = accountService.findAccountByEmailOrThrow(authentication.getName());
-        User user = userService.findUserByAccountIdOrThrow(account.getId());
-        UserDto userDto = userMapper.userToUserDto(userService.save(user, requestUpdateUserDto));
-
-        return ResponseEntity.ok(userDto);
     }
 
 }
