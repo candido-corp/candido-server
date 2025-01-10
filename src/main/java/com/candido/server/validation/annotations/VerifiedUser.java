246027ff1c6bdb59040d26a9cd293c1ed4e62b8c
Marco Tasca <tascamarcoo@gmail.com>
2025-01-10 22:53:15 +0100

    feat: Add custom annotation to check role of authenticated user

---
+++ src/main/java/com/candido/server/validation/annotations/VerifiedUser.java
@@ -0,0 +1,14 @@
+package com.candido.server.validation.annotations;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@PreAuthorize("hasRole('USER_VERIFIED') or hasRole('ADMIN')")
+public @interface VerifiedUser {
+}
