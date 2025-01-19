e44e225a7fbe2f83def89685b9cfbc2fc64c914e
Marco Tasca <tascamarcoo@gmail.com>
2025-01-19 21:35:56 +0100

    fix: Fix json properties in ResponseUserApplication

--- src/main/java/com/candido/server/dto/v1/response/application/ResponseUserApplication.java
+++ src/main/java/com/candido/server/dto/v1/response/application/ResponseUserApplication.java
@@ -1,16 +1,31 @@
 package com.candido.server.dto.v1.response.application;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class ResponseUserApplication {
+
+    @JsonProperty("application_id")
     private int applicationId;
+
+    @JsonProperty("filled_body")
     private String filledBody;
+
+    @JsonProperty("progress")
     private int progress;
+
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
+
+    @JsonProperty("deleted_at")
     private LocalDateTime deletedAt;
+
+    @JsonProperty("sent_at")
     private LocalDateTime sentAt;
 
     public ResponseUserApplication(
